43ed2e1d7207e999ac36a8e1b21b981003c6019d
argo-agorshechnikov <arkadiy.gorshechnikov@yandex.com>
2025-12-15 15:48:07 +0500

    static arr

--- Array/staticArr/staticArr.go
+++ Array/staticArr/staticArr.go
@@ -2,6 +2,71 @@ package main
 
 import "fmt"
 
+type StaticArr struct {
+	data   [5]int
+	length int
+}
+
+func NewStaticArr() *StaticArr {
+	return &StaticArr{
+		data:   [5]int{},
+		length: 0,
+	}
+}
+
+func (sa *StaticArr) Append(val int) bool {
+	if sa.length >= len(sa.data) {
+		return false
+	}
+
+	sa.data[sa.length] = val
+	sa.length++
+	return true
+}
+
+func (sa *StaticArr) Get(i int) (int, bool) {
+	if i < 0 || i >= sa.length {
+		return 0, false
+	}
+
+	return sa.data[i], true
+}
+
+func (sa *StaticArr) Set(i int, val int) bool {
+	if i < 0 || i >= sa.length {
+		return false
+	}
+
+	sa.data[i] = val
+	return true
+}
+
+func (sa *StaticArr) Length() int {
+	return sa.length
+}
+
+func (sa *StaticArr) Size() int {
+	return len(sa.data)
+}
+
 func main() {
-	fmt.Println("stat")
+	arr := NewStaticArr()
+	arr.Append(1)
+	arr.Append(2)
+	arr.Append(3)
+	arr.Append(4)
+	arr.Append(5)
+
+	for i := 0; i < arr.Length(); i++ {
+		v, _ := arr.Get(i)
+		fmt.Printf("index - %d, val - %d\n", i, v)
+	}
+
+	fmt.Println()
+
+	arr.Append(6)
+	for i := 0; i < arr.Length(); i++ {
+		v, _ := arr.Get(i)
+		fmt.Printf("index - %d, val - %d\n", i, v)
+	}
 }
