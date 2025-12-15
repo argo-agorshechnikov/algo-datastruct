f0b2eb3184db8c5ecfb5cf13c76e03d61dd80e7e
argo-agorshechnikov <arkadiy.gorshechnikov@yandex.com>
2025-12-15 16:06:58 +0500

    static matrix

---
+++ Array/matrix/staticMatrix/staticMatrix.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+// Статический двумерный массив
+
+type StaticMatrix struct {
+	data [3][3]int
+	rows int
+	cols int
+}
+
+func NewStaticMatrix() *StaticMatrix {
+	return &StaticMatrix{
+		data: [3][3]int{},
+		rows: 3,
+		cols: 3,
+	}
+}
+
+func (sm *StaticMatrix) Set(row, col, val int) bool {
+
+	if row < 0 || row >= sm.rows || col < 0 || col >= sm.cols {
+		return false
+	}
+
+	sm.data[row][col] = val
+	return true
+}
+
+func (sm *StaticMatrix) Get(row, col int) (int, bool) {
+	if row < 0 || row >= sm.rows || col < 0 || col >= sm.cols {
+		return 0, false
+	}
+
+	return sm.data[row][col], true
+}
+
+func (sm *StaticMatrix) Print() {
+	for i := 0; i < sm.rows; i++ {
+		for j := 0; j < sm.cols; j++ {
+			v, _ := sm.Get(i, j)
+			fmt.Print(v)
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	sMatrix := NewStaticMatrix()
+	sMatrix.Set(0, 0, 5)
+	sMatrix.Set(1, 2, 55)
+	sMatrix.Set(2, 2, 555)
+
+	sMatrix.Print()
+}
